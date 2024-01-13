ab266730dc450570bc628cc6120d6052eb056110
Luna <138384681+phLuna@users.noreply.github.com>
2024-01-12 23:06:37 -0300

    Update.

--- utills.py
+++ utills.py
@@ -1,4 +1,13 @@
 from src.database.connection import DatabaseManager
+from src.controller.people import create_person, read_people, update_person, delete_person
 
-tables = DatabaseManager()
-tables.create_tables()
+#create_person('Josias', '51997613120', '13593464730')
+
+#people = read_people()
+#for person in people:
+#    print(person.name)
+
+#person = update_people(1, 'Pedro Henrique')
+#print(person)
+
+#delete_person(1)
