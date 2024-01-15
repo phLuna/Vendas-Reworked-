076769b84e8b7813b984de108eccb4d908f1a2a6
Luna <138384681+phLuna@users.noreply.github.com>
2024-01-14 21:47:27 -0300

    Update.

--- utills.py
+++ utills.py
@@ -1,5 +1,7 @@
 from src.database.connection import DatabaseManager
 from src.controller.people import create_person, read_people, update_person, delete_person
+from src.controller.product import create_product, read_products, update_product, delete_product
+from src.controller.sell import create_sell, read_sells, update_sell, delete_sell
 
 #create_person('Josias', '51997613120', '13593464730')
 
@@ -11,3 +13,19 @@ from src.controller.people import create_person, read_people, update_person, del
 #print(person)
 
 #delete_person(1)
+
+#-----
+
+#create_product('Água', 5.99, '5555', 2)
+
+#products = read_products()
+#for product in products:
+#    print(product.name)
+
+#update_product(1, 'Carvão')
+
+#delete_product(1)
+
+#-----
+
+create_sell(2, 1, 3, 15.99)
