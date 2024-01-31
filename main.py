28e69f9e3ecbc0fa101d7448a0a3c2c58cebc378
Luna <138384681+phLuna@users.noreply.github.com>
2024-01-31 17:42:01 -0300

    Update.

--- main.py
+++ main.py
@@ -1,34 +1,14 @@
-from fastapi import FastAPI, HTTPException, status
+from fastapi import FastAPI, HTTPException, status, Path
+from pydantic import BaseModel
 import uvicorn
 
-from tables.people import create_person, read_people, update_person, delete_person
+from routes import clients_router
 
-app = FastAPI()
-
-@app.post("/people/add")
-def criar_pessoa(name: str, phone_number: str, cpf: str):
-    create_person(name, phone_number, cpf)
-    return {"message": f'{name} foi criado(a) com sucesso!'}
-
-@app.put("/people/update")
-def atualizar_pessoa(id: int, item: str, update: str):
-    response = update_person(id, item, update)
-    return response
-
-@app.get("/people/read")
-def ver_todos():
-    try:
-        people = read_people()
-        return people
-    except KeyError:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND, detail='Não encontrado!')
-    
-@app.delete("/people/delete")
-def deletar_pessoa(id:int):
-    response = delete_person(id)
-    return response
-    
+app = FastAPI(
+    title='Gerenciamento de uma loja.',
+    version='0.1',
+    description='Uma API para gerenciar as vendas de uma loja.')
+app.include_router(clients_router.router)
 
 if __name__ == '__main__':
     uvicorn.run(app)
