aa6f92a0fbbda891f0f3cd02fc84ab3c1b582ea9
Luna <138384681+phLuna@users.noreply.github.com>
2024-02-02 21:38:37 -0300

    Update.

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-from fastapi import FastAPI, HTTPException, status, Path
-from pydantic import BaseModel
+from fastapi import FastAPI
 import uvicorn
 
 from routes import clients_router, products_router, sells_router
@@ -8,9 +7,9 @@ app = FastAPI(
     title='Gerenciamento de uma loja.',
     version='0.1',
     description='Uma API para gerenciar as vendas de uma loja.')
-app.include_router(clients_router.router, tags=['Clients.'])
-app.include_router(products_router.router, tags=['Products.'])
-app.include_router(sells_router.router, tags=['Sells.'])
+app.include_router(clients_router.router, tags=['Clients'])
+app.include_router(products_router.router, tags=['Products'])
+app.include_router(sells_router.router, tags=['Sells'])
 
 if __name__ == '__main__':
     uvicorn.run(app)
